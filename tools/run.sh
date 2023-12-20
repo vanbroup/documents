b7b41be537408e0c2f133fe5b3144ab942669ad4
Paul van Brouwershaven <paul.vanbrouwershaven@entrust.com>
2023-12-20 13:47:35 +0100

    Update tohtml command in script

--- tools/run.sh
+++ tools/run.sh
@@ -7,7 +7,7 @@ rm -rf ../structured/*
 echo "Tranforming documents"
 
 py transform.py ../docs/BR.md
-py transform.py ../docs/EV.md
+py transform.py ../docs/EVG.md
 py transform.py ../docs/CS.md
 py transform.py ../docs/SMIME.md
 
@@ -18,11 +18,11 @@ py duplicates.py > ../duplicates.md
 echo "Building documents"
 
 py build.py BR
-py build.py EV
+py build.py EVG
 py build.py CS
 py build.py SMIME
 
 mkdir -p ../public
-python3 tohtml.py > ../public/index.html
+py tohtml.py -out ../public/index.html
 
 echo "Done"
