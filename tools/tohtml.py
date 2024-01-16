8fc80480aaeeddc9c5ad724e4e66b1a408461ee1
Paul van Brouwershaven <paul.vanbrouwershaven@entrust.com>
2024-01-16 18:01:29 +0100

    Include document diff

--- tools/tohtml.py
+++ tools/tohtml.py
@@ -4,9 +4,12 @@ import os
 import argparse
 from pathlib import Path
 import markdown
+from diff_match_patch import diff_match_patch
 
 # Define document types as constants
-DOCUMENT_TYPES = ['BR', 'CS', 'SMIME', 'EVG', 'TLS']
+output_folder = '../output/'
+DOCUMENT_TYPES = [os.path.splitext(file)[0] for file in os.listdir(output_folder) if file.endswith('.md')]
+
 
 def get_section(file: Path) -> str:
     return ".".join([str(item).lstrip("0") for item in re.findall("([0-9]{3}|[0-9]{2}[A-Z]) ", file.resolve().__str__())])
@@ -28,8 +31,8 @@ def main():
         description='Remove files that have the same content as the Baseline Requirements')
     parser.add_argument('-in', '--input', default='../structured/', type=str,
                         help='input directory')
-    parser.add_argument('-out', '--output', default='output.html', type=str,
-                        help='output file name')
+    parser.add_argument('-out', '--output', default='../public/', type=str,
+                        help='output directory')
     args = parser.parse_args()
 
     sections = {}
@@ -73,6 +76,8 @@ def main():
             # Get the template
             template_file = os.path.join(dirpath, filename)
             relative_template_file = os.path.relpath(template_file, 'templates')
+            relative_template_file = os.path.normpath(relative_template_file)
+            relative_template_file = relative_template_file.replace("\\", "/") 
             template = env.get_template(relative_template_file)
 
             # Render the template with data
@@ -92,6 +97,49 @@ def main():
             # Write the output to a file in the output directory
             with open(os.path.join(output_dir, filename), 'w', encoding='utf-8') as f:
                 f.write(output)
+
+
+    template = env.get_template('includes/diff.html')
+
+    # Writing a document diff between docs source and final build
+    dmp = diff_match_patch()
+
+    for dt in DOCUMENT_TYPES:
+        try:
+            with open('../docs/'+ dt + '.md', 'r', encoding='utf-8') as source_file:
+                with open('../output/'+ dt + '.md', 'r', encoding='utf-8') as build_file:
+                    source_content = source_file.read()
+                    build_content = build_file.read()
+                    if source_content != build_content:
+                        diffs = dmp.diff_main(source_content, build_content)
+                        dmp.diff_cleanupSemantic(diffs)
+
+                        diff_html = dmp.diff_prettyHtml(diffs)
+
+                        # Calculate similarity
+                        levenshtein = dmp.diff_levenshtein(diffs)
+                        diff_similarity = (1 - levenshtein / max(len(source_content), len(build_content))) * 100
+
+                    else:
+                        diff_html = "The input and output are exactly the same."
+
+                    rendered_html = template.render(
+                        document_types=DOCUMENT_TYPES,
+                        document_type=dt,
+                        markdown_to_html=markdown_to_html,
+                        source_content=source_content,
+                        build_content=build_content,
+                        diff_text=diff_html,
+                        diff_similarity=diff_similarity,
+                        range=range  # Pass the range function to the template
+                    )
+
+                    with open(args.output+'/diff-'+ dt.lower() +'.html', 'w', encoding='utf-8') as output_file:
+                        output_file.write(rendered_html)
+        except FileNotFoundError as e:
+            print(f"File not found: {e.filename}")
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
 
 
 if __name__ == '__main__':
