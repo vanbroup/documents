274cb4ea427b08ef1902db4b355b73adcd7cd964
Paul van Brouwershaven <paul.vanbrouwershaven@entrust.com>
2024-02-02 17:50:35 +0100

    Add debugging

--- tools/run.sh
+++ tools/run.sh
@@ -12,15 +12,15 @@ python transform.py ../docs/SMIME.md
 echo "::endgroup::"
 
 echo "::group::Change known BR TLS specific files to TLS"
-find ../structured/APPENDIX/ -type f -name "000_BR_*.md" -exec sh -x -c 'mv "$1" "${1//_BR_/_TLS_}"' _ {} \;
+find ../structured/APPENDIX/ -type f -name "000_BR_*.md" -exec bash -x -c 'mv -v "$1" "${1//_BR_/_TLS_}"' _ {} \;
 echo "::endgroup::"
 
 echo "::group::Remove duplicates"
-python duplicates.py > ../duplicates.md
+python duplicates.py | tee ../duplicates.md
 echo "::endgroup::"
 
 echo "::group::Add examples"
-cp -r testlayers/* ../structured/
+cp -rv testlayers/* ../structured/
 echo "::endgroup::"
 
 echo "::group::Build documents"
@@ -35,12 +35,12 @@ python totls.py
 echo "::endgroup::"
 
 echo "::group::Using BR main document as a template for TLS"
-cp ../structured/000_BR.md ../structured/000_TLS.md
+cp -v ../structured/000_BR.md ../structured/000_TLS.md
 sed -i 's/Certificates/TLS Certificates/g' ../structured/000_TLS.md
 echo "::endgroup::"
 
 echo "::group::Remove output"
-rm -f ../output/*
+rm -fv ../output/*
 echo "::endgroup::"
 
 echo "::group::Building documents again"
